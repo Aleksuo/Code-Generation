401c7739f72aa08414cf8f23723a9883f09543d1
Sopulius <aleksi.suoranta@gmail.com>
2018-04-11 01:06:09 +0300

    token types added

--- MiniPascal/FrontEnd/LexicalAnalysis/TokenType.cs
+++ MiniPascal/FrontEnd/LexicalAnalysis/TokenType.cs
@@ -8,6 +8,16 @@ namespace MiniPascal.FrontEnd.LexicalAnalysis
 {
     public enum TokenType
     {
+        //special symbols 
+        PLUS, MINUS, MULT, MOD, EQUALS, NOTEQUALS, LESS, GREATER, LESSOREQUAL, GREATEROREQUAL,
+        LEFTCURLY, RIGHTCURLY, LEFTSQUARE, RIGHTSQUARE, DOT, COMMA, SEMICOLON, COLON, 
+
         //Keywords
+        OR, AND, NOT, IF, THEN, ELSE, OF, WHILE, DO, BEGIN, END, VAR,
+        ARRAY, PROCEDURE, FUNCTION, PROGRAM, ASSERT, RETURN,
+
+        //Predefined ids
+        TYPE, FALSE, TRUE, READ, SIZE, WRITELN,
+
     }
 }
